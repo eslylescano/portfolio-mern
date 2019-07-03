809577694a8642a60c274be0d1ca3fcfed5846da
Esly Adán <eslylescano@gmail.com>
2019-07-03 02:23:51 +0100

    creating token when user is registered

--- routes/api/user.js
+++ routes/api/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route     GET api/users
 // @desc      Register user
@@ -41,7 +43,7 @@ router.post('/', [
             let user = await User.findOne({ email });
 
             if (user) {
-                return sres.status(400).json({ errors: [{ msg: 'User already exists' }] });
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
             }
 
             user = new User({
@@ -65,9 +67,25 @@ router.post('/', [
             user.password = await bcrypt.hash(password, salt);
 
             await user.save();
-            // Return jsonwebtoken
 
-            res.send('User registered');
+
+            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'), { expiresIn: 60 * 60 * 10 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                });
+
+
+
         } catch (err) {
             console.error(err.message);
             res.status(500).send('server error');
